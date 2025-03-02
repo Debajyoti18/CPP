bf3728d3335a2b71935948f6ce1e1e5437c742b3
DebajyotiBhatta <bdebajyoti22@gmail.com>
2025-03-02 14:58:41 +0000

    basics of c++

---
+++ 01_basics_Io.cpp
@@ -0,0 +1,27 @@
+#include<iostream> //header files and libraries
+#include<bits/stdc++.h> // contains all the libraries
+
+using namespace std;
+/*This defines a scope for the identifiers that are used in a program. For using the identifiers defined in the namespace scope
+ we must include the using directive.This will bring all the identifiers defined in std to the current global scope. */
+
+
+int main(){
+    // std::cout << "Hello I'm Dj !" << "\n";
+    // std::cout << "Hello I'm Dj !" << std::endl;
+    //"/n" and endl are commonly used for the next line 
+    cout << "Hello I'm Dj !" << "\n";
+    cout << "Hello I'm Dj !" << endl;//The operator << is called the insertion or put to operator. It inserts
+    //(or sends) the contents of the variable on its right to the object on its left
+
+    //input 
+    int x,y;
+    cin >> x >> y;// The operator >> is known as extraction or get from operator. It extracts (or takes) the value from
+     //the keyboard and assigns it to the variable on its right .: cin can read only one word and therefore we cannot
+    //use names with blank spaces
+    cout << "Value of the x is " << x << " Y is :" << y;
+
+
+    return 0;//The default return type for all functions in C++ is int. So its return to the os. if no return then shows warnings.
+}
+/*>>>>> Theory Concepts >>>>>>>>>*/
