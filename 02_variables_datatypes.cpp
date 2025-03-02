f2668a5adb3ea9c54dd9252ffe2681faeaa3f76d
DebajyotiBhatta <bdebajyoti22@gmail.com>
2025-03-02 15:33:21 +0000

    variables and datatypes

---
+++ 02_variables_datatypes.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    //user defind-structure,union,class,enumeration
+    //built-in - int , char ,void , float,double
+    //derived - array , function, pointer, reference
+
+    //int x = 10;
+    long x =  19223;
+    long long y = 192239398;
+
+    //Strings 
+     string s1,s2 ;
+    // cin >>  s1  >> s2 ;
+    // cout <<s1 << s2;
+    getline(cin , s1);//picks up till the line breaks.
+
+    //char ch ='a';
+    bool  x = true;
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+
+}
