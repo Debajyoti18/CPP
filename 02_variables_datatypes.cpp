b4845521e52ed18b343e4370eaf3b554bdf953ac
DebajyotiBhatta <bdebajyoti22@gmail.com>
2025-03-25 16:30:21 +0000

    adding variables

--- 02_variables_datatypes.cpp
+++ 02_variables_datatypes.cpp
@@ -1,4 +1,5 @@
-#include<bits/stdc++.h>
+#include<iostream>
+#include<string>
 using namespace std;
 
 int main(){
@@ -9,12 +10,14 @@ int main(){
     //int x = 10;
     // long x =  19223;
     long long y = 192239398;
-
+    double point = 90.9788;//= 90.9788 precision upto 4 place after decimal then round off
+        point = 90.97889;//= 90.9789
+    float  temp = 73.97282682;//73.9728 4th place 
     //Strings 
      string s1,s2 ;
     // cin >>  s1  >> s2 ;
     // cout <<s1 << s2;
-    getline(cin , s1);//picks up till the line breaks.
+    // getline(cin , s1);//picks up till the line breaks.
 
     //char ch ='a';
     bool  x = true;//nonempty gives output as the 1
@@ -22,6 +25,21 @@ int main(){
     bool c = 5;//1
     //c = '';//not allowed
     // bool x = false;//0 and x=0 gives 0
+
+
+    // char mychar = 'Abb';//overflow in conversion from ‘int’ to ‘char’ changes value from ‘4285026’ to ‘'b'’ [-Woverflow]
+   char  mychar = 'A';//A takes only one character at a time 
+
+   unsigned int positive = -11090;//usigned make it to positive and over flow happen
+    signed int negative = -1882;//no overflow due to signed
+
+
+
+
+
+
+    cout << negative << endl ;
+
     
 
 
