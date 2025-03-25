bb30339ff25fbe795ee2e3aaef6d761b459bb67b
DebajyotiBhatta <bdebajyoti22@gmail.com>
2025-03-25 14:54:01 +0000

    adding namespace

--- 01_basics_Io.cpp
+++ 01_basics_Io.cpp
@@ -1,12 +1,19 @@
 #include<iostream> //header files and libraries
 #include<bits/stdc++.h> // contains all the libraries
-
+//c++ is statically typed
 using namespace std;
 /*This defines a scope for the identifiers that are used in a program. For using the identifiers defined in the namespace scope
  we must include the using directive.This will bring all the identifiers defined in std to the current global scope. */
 
 
+ namespace myspaced {
+    void display(){
+        cout<< "hello this is my region";
+    }
+ }
+
 int main(){
+    myspaced:: display;
     // std::cout << "Hello I'm Dj !" << "\n";
     // std::cout << "Hello I'm Dj !" << std::endl;
     //"/n" and endl are commonly used for the next line 
